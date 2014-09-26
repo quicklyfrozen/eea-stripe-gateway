f8dcdb26675a20951e7d1571bf5340db6065f3e7
Brent Christensen <brent@eventespresso.com>
2014-09-26 15:52:29 -0700

    refactor JS to work with SPCO [touch:6776]

--- scripts/espresso_stripe_onsite.js
+++ scripts/espresso_stripe_onsite.js
@@ -1,32 +1,30 @@
 jQuery(document).ready(function($) {
 
-	$('#ee-spco-payment_options-reg-step-form').submit( function() {
-		var $form = $(this);
-
-		// Disable the submit button to prevent repeated clicks
-		$form.find('button').prop('disabled', true);
-		Stripe.card.createToken($form, stripeResponseHandler);
-
-		// Prevent the form from submitting with the default action
-		return false;
+    SPCO.main_container.on( 'click', '.stripe-button', function() {
+        e.preventDefault();
+        e.stopPropagation();
+        // Disable the submit button to prevent repeated clicks
+        SPCO.disable_submit_buttons();
+        Stripe.card.createToken( SPCO.current_form_to_validate, stripeResponseHandler );
 	});
 
 });
 
 function stripeResponseHandler( status, response ) {
-	var $form = jQuery("#ee-spco-payment_options-reg-step-form");
+
+	var $form = SPCO.current_form_to_validate;
 
 	if ( response.error ) {
 		// Show the errors on the form.
-		$form.find('.payment-errors').text(response.error.message);
-		$form.find('button').prop('disabled', false);
+        SPCO.current_form_to_validate.find('.payment-errors').text(response.error.message);
+        SPCO.current_form_to_validate.find('button').prop('disabled', false);
 	} else {
 		// Token contains id, last4, and card type.
 		var token = response.id;
 		// Insert the token into the form so it gets submitted to the server
-		$form.append($('<input type="hidden" name="ee-stripe-token" />').val(token));
-		$form.append('<input type="hidden" name="stripe-token" value="' + token + '" />');
+        SPCO.current_form_to_validate.append($('<input type="hidden" name="ee-stripe-token" />').val(token));
+        SPCO.current_form_to_validate.append('<input type="hidden" name="stripe-token" value="' + token + '" />');
 		// and re-submit.
-		$form.submit();
+        SPCO.current_form_to_validate.submit();
 	}
 }
