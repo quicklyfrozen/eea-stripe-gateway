5f2328a1ad5bab971b294f30ebaf0d830a0e8c2f
Brent Christensen <brent@eventespresso.com>
2014-12-03 12:33:10 -0800

    move all functionality into class; add checks for required  SPCO and StripeCheckout classes; [touch:6776]

--- scripts/espresso_stripe_onsite.js
+++ scripts/espresso_stripe_onsite.js
@@ -1,77 +1,107 @@
 jQuery(document).ready(function($) {
-   /* SPCO.main_container.on( 'click', '#spco-go-to-step-finalize_registration-submit', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        // Disable the submit button to prevent repeated clicks
-        SPCO.disable_submit_buttons();
-        var $form = $('#ee-Stripe-billing-form');
-        Stripe.setPublishableKey('YOUR_PUBLISHABLE_KEY');
-        Stripe.card.createToken({
-                number: $form.find('.card-number').val(),
-                cvc: $form.find('.card-cvc').val(),
-                exp_month: $form.find('.card-expiry-month').val(),
-                exp_year: $form.find('.card-expiry-year').val()
-            },
-            stripeResponseHandler
-        );
-	});
+
+	var EE_STRIPE;
+
+	/**
+	 * @namespace EE_STRIPE
+	 * @type {{
+		 *     StripeCheckout: object,
+		 *     handler: object
+		 *     token: object
+	 * }}
+	 * @namespace transaction_args
+	 * @type {{
+	 *     data_key: string,
+	 *     data_name: string
+	 *     data_image: string
+	 *     data_cc_number: number
+	 *     data_exp_month: number
+	 *     data_exp_year: number
+	 *     data_cvc: number
+	 *     accepted_message: string
+	 *     no_SPCO_error: string
+	 *     no_StripeCheckout_error: string
+	 * }}
+	 */
+	EE_STRIPE = {
+
+		handler : {},
+		submit_payment_button : $('#custom-stripe-button'),
 
 
-    function stripeResponseHandler( status, response ) {
-        alert('stripeResponseHandler');
 
-        if ( response.error ) {
-            // Show the errors on the form.
-            SPCO.hide_notices();
-            var msg = SPCO.generate_message_object( '', SPCO.tag_message_for_debugging( 'stripeResponseHandler error', response.error.message ), '' );
-            SPCO.scroll_to_top_and_display_messages( SPCO.main_container, msg );
-            SPCO.enable_submit_buttons();
-        } else {
-            alert('NO errors!');
+		/**
+		 * @function init		 *
+		 */
+		init : function() {
+			// ensure that the SPCO js class is loaded
+			if ( typeof SPCO === 'undefined' ) {
+				SPCO.scroll_to_top_and_display_messages( SPCO.main_container, SPCO.generate_message_object( transaction_args.no_SPCO_error, '', '' ));
+				return;
+			}
+			// ensure that the StripeCheckout js class is loaded
+			if ( typeof StripeCheckout === 'undefined' ) {
+				SPCO.scroll_to_top_and_display_messages( SPCO.main_container, SPCO.generate_message_object( transaction_args.no_StripeCheckout_error, '', '' ));
+				return;
+			}
+			EE_STRIPE.set_up_handler();
+			EE_STRIPE.set_listener_for_payment_method_selector();
+			EE_STRIPE.set_listener_for_submit_payment_button();
+		},
 
-            // set token value in hidden input so it gets submitted to the server
-            $('#ee-stripe-token').val( response.id ) ;
-            // and re-submit by triggering a click on the current step's submit button
-            //SPCO.current_form_to_validate.find('.spco-next-step-btn').trigger('click');
-        }
-    }*/
-    
-    var defaultButtonColor = '';
 
-    SPCO.main_container.on( 'click', '#ee-available-payment-method-inputs-stripe_onsite', function(e) {
-    	// Deactivate SPCO submit buttons to prevent submitting with no Stripe token.
-		SPCO.disable_submit_buttons();
 
-		/*defaultButtonColor = $('.spco-next-step-btn').style.backgroundColor;
-		$('.spco-next-step-btn').each( function() {
-			$(this).style.background = '#A0A0A0';
-		});*/
-    });
+		/**
+		 * @function set_up_handler
+		 */
+		set_up_handler : function() {
+			EE_STRIPE.handler = StripeCheckout.configure({
+				key: transaction_args.data_key,
+				image: transaction_args.data_image,
+				token: function( token ) {
+					// Use the token to create the charge with a server-side script.
+					$('#ee-stripe-token').val( token.id );
+				}
+			});
+		},
 
-	var handler = StripeCheckout.configure({
-		key: transaction_args.data_key,
-		image: transaction_args.data_image,
-		token: function(token) {
-			// Use the token to create the charge with a server-side script.
-			$('#ee-stripe-token').val( token.id );
+
+
+		/**
+		 * @function set_listener_for_payment_method_selector
+		 */
+		set_listener_for_payment_method_selector : function() {
+			SPCO.main_container.on( 'click', '#ee-available-payment-method-inputs-stripe_onsite', function(e) {
+				// Deactivate SPCO submit buttons to prevent submitting with no Stripe token.
+				SPCO.disable_submit_buttons();
+			});
+		},
+
+
+
+		/**
+		 * @function set_listener_for_submit_payment_button
+		 */
+		set_listener_for_submit_payment_button : function() {
+			SPCO.main_container.on( 'click', '#custom-stripe-button', function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+				// Open Checkout with further options.
+				EE_STRIPE.handler.open({
+					name: transaction_args.data_name,
+					amount: $('#ee-stripe-transaction-total').val(),
+					description: $('#ee-stripe-prod-description').val()
+				});
+				// Enable SPCO submit buttons.
+				SPCO.enable_submit_buttons();
+				EE_STRIPE.submit_payment_button.disabled = true;
+				EE_STRIPE.submit_payment_button.val( transaction_args.accepted_message );
+			});
 		}
-	});
 
-	SPCO.main_container.on( 'click', '#custom-stripe-button', function(e) {
-		// Open Checkout with further options.
-		handler.open({
-			name: transaction_args.data_name,
-			amount: $('#ee-stripe-transaction-total').val(),
-			description: $('#ee-stripe-prod-description').val()
-		});
-		// Enable SPCO submit buttons.
-		SPCO.enable_submit_buttons();
-		/*$('.spco-next-step-btn').each( function() {
-			$(this).style.background = backgroundColor;
-		});
-		$('#custom-stripe-button').disabled = true;
-		$('#custom-stripe-button').val('Accepted. Click "Finalize Registration"');*/
-		e.preventDefault();
-	});
+
+	};
+
+	EE_STRIPE.init();
 
 });
