82454bf93a7e92c1d27730aceba094e99c522a0b
Brent Christensen <brent@eventespresso.com>
2014-12-03 12:56:32 -0800

    refactor Stripe JS class to include Nazar's latest changes [touch:6776]

--- scripts/espresso_stripe_onsite.js
+++ scripts/espresso_stripe_onsite.js
@@ -5,9 +5,19 @@ jQuery(document).ready(function($) {
 	/**
 	 * @namespace EE_STRIPE
 	 * @type {{
-		 *     StripeCheckout: object,
+		 *     handler: object,
+		 *     submit_payment_button: object,
+		 *     error_msg: string,
+	 * }}
+	 * @namespace StripeCheckout
+	 * @type {{
+		 *     configure: function,
 		 *     handler: object
-		 *     token: object
+	 * }}
+	 * @namespace token
+	 * @type {{
+		 *     error: object
+		 *     id: string
 	 * }}
 	 * @namespace transaction_args
 	 * @type {{
@@ -19,6 +29,7 @@ jQuery(document).ready(function($) {
 	 *     data_exp_year: number
 	 *     data_cvc: number
 	 *     accepted_message: string
+	 *     card_error_message: string
 	 *     no_SPCO_error: string
 	 *     no_StripeCheckout_error: string
 	 * }}
@@ -27,21 +38,24 @@ jQuery(document).ready(function($) {
 
 		handler : {},
 		submit_payment_button : $('#custom-stripe-button'),
+		error_msg : '',
 
 
 
 		/**
-		 * @function init		 *
+		 * @function init
 		 */
 		init : function() {
 			// ensure that the SPCO js class is loaded
 			if ( typeof SPCO === 'undefined' ) {
-				SPCO.scroll_to_top_and_display_messages( SPCO.main_container, SPCO.generate_message_object( transaction_args.no_SPCO_error, '', '' ));
+				EE_STRIPE.error_msg = SPCO.generate_message_object( SPCO.tag_message_for_debugging( 'EE_STRIPE.init() error', transaction_args.no_SPCO_error ), '', '' );
+				SPCO.scroll_to_top_and_display_messages( SPCO.main_container, EE_STRIPE.error_msg );
 				return;
 			}
 			// ensure that the StripeCheckout js class is loaded
 			if ( typeof StripeCheckout === 'undefined' ) {
-				SPCO.scroll_to_top_and_display_messages( SPCO.main_container, SPCO.generate_message_object( transaction_args.no_StripeCheckout_error, '', '' ));
+				EE_STRIPE.error_msg = SPCO.generate_message_object( SPCO.tag_message_for_debugging( 'EE_STRIPE.init() error', transaction_args.no_StripeCheckout_error ), '', '' );
+				SPCO.scroll_to_top_and_display_messages( SPCO.main_container, EE_STRIPE.error_msg );
 				return;
 			}
 			EE_STRIPE.set_up_handler();
@@ -58,9 +72,19 @@ jQuery(document).ready(function($) {
 			EE_STRIPE.handler = StripeCheckout.configure({
 				key: transaction_args.data_key,
 				image: transaction_args.data_image,
-				token: function( token ) {
+				token: function(token) {
 					// Use the token to create the charge with a server-side script.
-					$('#ee-stripe-token').val( token.id );
+					if ( token.error ) {
+						SPCO.hide_notices();
+						EE_STRIPE.error_msg = SPCO.generate_message_object( SPCO.tag_message_for_debugging( 'stripeResponseHandler error', token.error.message ), '', '' );
+						SPCO.scroll_to_top_and_display_messages( SPCO.main_container, EE_STRIPE.error_msg );
+						EE_STRIPE.submit_payment_button.text( transaction_args.card_error_message );
+					} else {
+						$('#ee-stripe-token').val( token.id );
+						// Enable SPCO submit buttons.
+						SPCO.enable_submit_buttons();
+						EE_STRIPE.submit_payment_button.text( transaction_args.accepted_message ).css('background', '#1A89C8').prop('disabled', true);
+					}
 				}
 			});
 		},
@@ -71,9 +95,11 @@ jQuery(document).ready(function($) {
 		 * @function set_listener_for_payment_method_selector
 		 */
 		set_listener_for_payment_method_selector : function() {
-			SPCO.main_container.on( 'click', '#ee-available-payment-method-inputs-stripe_onsite', function(e) {
+			SPCO.main_container.on( 'click', '.spco-next-step-btn', function() {
 				// Deactivate SPCO submit buttons to prevent submitting with no Stripe token.
-				SPCO.disable_submit_buttons();
+				if ( EE_STRIPE.submit_payment_button.length > 0 && EE_STRIPE.submit_payment_button.val().length <= 0 ) {
+					SPCO.disable_submit_buttons();
+				}
 			});
 		},
 
@@ -92,10 +118,6 @@ jQuery(document).ready(function($) {
 					amount: $('#ee-stripe-transaction-total').val(),
 					description: $('#ee-stripe-prod-description').val()
 				});
-				// Enable SPCO submit buttons.
-				SPCO.enable_submit_buttons();
-				EE_STRIPE.submit_payment_button.disabled = true;
-				EE_STRIPE.submit_payment_button.val( transaction_args.accepted_message );
 			});
 		}
 
