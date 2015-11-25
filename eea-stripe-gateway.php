1389394321120aef904f64f2b4b7fc12ad161ce1
EE DevBox Server <admin@eventespresso.com>
2015-11-25 14:03:34 -0500

    Bumping version to 1.0.12.rc.002

--- eea-stripe-gateway.php
+++ eea-stripe-gateway.php
@@ -4,7 +4,7 @@
   Plugin URI: http://www.eventespresso.com
   Description: The Event Espresso Stripe Gateway adds a new onsite payment method.
 
-  Version: 1.0.12.rc.001
+  Version: 1.0.12.rc.002
 
   Author: Event Espresso
   Author URI: http://www.eventespresso.com
@@ -38,7 +38,7 @@
  *
  * ------------------------------------------------------------------------
  */
-define( 'EE_STRIPE_VERSION', '1.0.12.rc.001' );
+define( 'EE_STRIPE_VERSION', '1.0.12.rc.002' );
 define( 'EE_STRIPE_PLUGIN_FILE',  __FILE__ );
 
 function load_espresso_stripe() {
