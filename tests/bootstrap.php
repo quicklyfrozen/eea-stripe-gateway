8b06f0fbba75ab07d0b2dbe79f83d8e6c5a33efd
Darren Ethier <darren@roughsmootheng.in>
2017-04-22 16:44:23 -0400

    update test bootstrap/config for stripe payment method add-on [touch: 10623]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,11 +6,15 @@
 use EETests\bootstrap\AddonLoader;
 
 $core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+//if still don't have $core_tests_dir, then let's check tmp folder.
+if (! is_dir($core_tests_dir)) {
+    $core_tests_dir = '/tmp/event-espresso-core/tests/';
+}
 require $core_tests_dir . 'includes/CoreLoader.php';
 require $core_tests_dir . 'includes/AddonLoader.php';
 
 define('EEA_STRIPE_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
-define('EEA_STRIPE_TESTS_DIR', EEA_STRIPE_PLUGIN_DIR . 'tests');
+define('EEA_STRIPE_TESTS_DIR', EEA_STRIPE_PLUGIN_DIR . 'tests/');
 
 
 $addon_loader = new AddonLoader(
