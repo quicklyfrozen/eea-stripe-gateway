425cf4ae1e01cd87eab30b2d113fcf11e683b492
Tony Warwick <tony@eventespresso.com>
2017-04-25 01:30:49 +0100

    Add additional help text for the new locale option;
    [touch: 10115]

--- payment_methods/Stripe_Onsite/help_tabs/payment_methods_overview_stripe.help_tab.php
+++ payment_methods/Stripe_Onsite/help_tabs/payment_methods_overview_stripe.help_tab.php
@@ -30,6 +30,14 @@
 		<?php esc_html_e( 'Specify whether Checkout should collect the user\'s billing address.', 'event_espresso' ); ?>
 	</li>
 	<li>
+		<strong><?php esc_html_e( 'Checkout Locale', 'event_espresso' ); ?></strong><br />
+		<?php printf( esc_html__( 'Select the language Stripe should use for checkout here, use auto for Stripe to determine which of its %1$ssupported languages%2$s to use or choose a specific specific language.', 'event_espresso' ),
+			'<a href="https://stripe.com/docs/checkout#supported-languages" target="_blank">',
+			'</a>' 
+		);
+		?>
+	</li>
+	<li>
 		<strong><?php esc_html_e( 'Logo URL', 'event_espresso' ); ?></strong><br />
 		<?php esc_html_e( 'Upload a logo that will appear at the top of the Stripe checkout, for best results use 125px by 125px.', 'event_espresso' ); ?>
 	</li>
